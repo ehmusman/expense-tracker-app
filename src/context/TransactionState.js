e9c616df4b8687d570cb3fd771afe06d4eb7ba90
ehmusman <ehmusman@gmail.com>
2020-12-14 10:01:54 +0500

    initial transaction data is removed

--- src/context/TransactionState.js
+++ src/context/TransactionState.js
@@ -6,12 +6,7 @@ import { ADD_TRANSACTION, DELETE_TRANSACTION } from './types'
 
 const TransactionState = (props) => {
     const initialState = {
-        transactions: [
-            { id: 1, text: 'Flower', amount: -20 },
-            { id: 2, text: 'Salary', amount: 300 },
-            { id: 3, text: 'Book', amount: -10 },
-            { id: 4, text: 'Camera', amount: 150 }
-        ]
+        transactions: []
     }
 
     const addTransaction = (data) => {
